c09420c20cc5b5e8d13ea2630017e75ef3ea8278
Abin Simon <abinsimon10@gmail.com>
2017-03-12 09:07:36 +0530

    remove some black fg colors

--- .tmux/themes/theme.sh
+++ .tmux/themes/theme.sh
@@ -6,30 +6,30 @@ set -g pane-active-border-fg brightred
 
 # status line
 set -g status-justify left
-set -g status-bg colour8
+set -g status-bg colour232
 set -g status-fg colour12
 set -g status-interval 1
 
 # messaging
-set -g message-fg black
+set -g message-fg white
 set -g message-bg yellow
 set -g message-command-fg blue
 set -g message-command-bg black
 
 #window mode
 setw -g mode-bg colour6
-setw -g mode-fg colour0
+setw -g mode-fg colour232
 
 # window status
 setw -g window-status-format " #F#I:#W#F "
 setw -g window-status-current-format " #F#I:#W#F "
-setw -g window-status-format "#[fg=magenta]#[bg=colour8] #I #[bg=colour8]#[fg=colour231] #W "
-setw -g window-status-current-format "#[bg=color8]#[fg=colour231] #I #[fg=colour231]#[bg=colour8] #W "
-setw -g window-status-current-bg colour0
+setw -g window-status-format "#[fg=magenta]#[bg=colour232] #I #[bg=colour232]#[fg=colour231] #W "
+setw -g window-status-current-format "#[bg=color8]#[fg=colour231] #I #[fg=colour231]#[bg=colour232] #W "
+setw -g window-status-current-bg colour232
 setw -g window-status-current-fg colour11
 setw -g window-status-current-attr dim
 setw -g window-status-bg green
-setw -g window-status-fg black
+setw -g window-status-fg white
 setw -g window-status-attr reverse
 
 # loud or quiet?
@@ -48,35 +48,35 @@ setw -g mode-fg colour196
 setw -g mode-bg colour238
 
 # pane
-set -g pane-border-bg colour8
+set -g pane-border-bg colour232
 set -g pane-border-fg colour238
-set -g pane-active-border-bg colour8
+set -g pane-active-border-bg colour232
 set -g pane-active-border-fg colour51
 
 # statusbar
 set -g status-position top
-set -g status-bg colour8
+set -g status-bg colour232
 set -g status-fg colour137
 set -g status-attr dim
 
 setw -g window-status-current-fg colour81
-setw -g window-status-current-bg colour8
+setw -g window-status-current-bg colour232
 setw -g window-status-current-attr bold
 setw -g window-status-current-format ' #I#[fg=colour250]:#[fg=colour255]#W#[fg=colour50]#F '
 
 setw -g window-status-fg colour138
-setw -g window-status-bg colour8
+setw -g window-status-bg colour232
 setw -g window-status-attr none
 setw -g window-status-format ' #I#[fg=colour237]:#[fg=colour250]#W#[fg=colour244]#F '
 
 setw -g window-status-bell-attr bold
 setw -g window-status-bell-fg colour255
-setw -g window-status-bell-bg colour8
+setw -g window-status-bell-bg colour232
 
 # messages
 set -g message-attr bold
 set -g message-fg colour232
-set -g message-bg colour8
+set -g message-bg colour232
 
 # status bar left and right
 tm_color_active=colour39
@@ -89,7 +89,7 @@ tm_battery="#(~/bin/battery_indicator.sh)"
 tm_date="#[fg=$tm_color_inactive] %R %d %b"
 tm_host="#[fg=$tm_color_feature,bold]#h"
 tm_session_name="$tm_icon #S"
-set -g status-left '#[fg=colour231,bg=colour8,bold] *meain* #[bg=black] '
-set -g status-right $tm_tunes' #[fg=colour15,bg=colour8,bold] '$tm_battery'#[fg=colour15,bg=colour8,bold] %d/%m #[fg=colour15,bg=colour8,bold] %r |'$tm_session_name' '
+set -g status-left '#[fg=colour231,bg=colour232,bold] *meain* #[bg=black] '
+set -g status-right $tm_tunes' #[fg=colour15,bg=colour232,bold] '$tm_battery'#[fg=colour15,bg=colour232,bold] %d/%m #[fg=colour15,bg=colour232,bold] %r |'$tm_session_name' '
 set -g status-right-length 100
 set -g status-left-length 20
