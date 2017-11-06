a8ed3037162ec36c49e2dec4868c15f3b1199847
Abin Simon <abinsimon10@gmail.com>
2017-11-06 20:14:45 +0530

    [scripts] better music.js

--- scripts/bin/music.js
+++ scripts/bin/music.js
@@ -1,13 +1,11 @@
 let output = '';
-if (Application('Spotify').running()) {
-    const track = Application('Spotify').currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = (`♫ ${title} - ${artist}`).substr(0, 50);
-} else if (Application('iTunes').running()) {
-    const track = Application('iTunes').currentTrack;
-    const artist = track.artist();
-    const title = track.name();
-    output = (`♫ ${title} - ${artist}`).substr(0, 50);
+const musicSources = ['Spotify', 'iTunes']
+for(source in musicSources){
+    if ( Application(musicSources[source]).running() ){
+        const track = Application(musicSources[source]).currentTrack;
+        const artist = track.artist();
+        const title = track.name();
+        output = (`♫ ${title} - ${artist}`).substr(0, 50);
+        break
+    }
 }
-output;
