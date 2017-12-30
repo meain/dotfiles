e829be7b93d0ecc7c3b9a360d812ffcf6e1a5366
Abin Simon <abinsimon10@gmail.com>
2017-12-30 11:15:13 +0530

    [tmux] nicer battery indicator in status bar (with icons)

---
+++ scripts/bin/mini_batt.sh
@@ -0,0 +1,18 @@
+VALUE=$(pmset -g batt | grep InternalBattery | awk '{print $3}' | sed 's:..$::g')
+SOURCE=$(pmset -g batt | grep 'Now drawing from' | awk '{print $4}' | sed 's:^.::g')
+#   i      
+BATTERY_ICON=" "
+if [ $VALUE -lt 20 ];then
+  BATTERY_ICON=" "
+elif [ $VALUE -lt 40 ];then
+  BATTERY_ICON=" "
+elif [ $VALUE -lt 70 ];then
+  BATTERY_ICON=" "
+elif [ $VALUE -lt 90 ];then
+  BATTERY_ICON=" "
+fi
+if [ "$SOURCE" = "AC" ]; then
+  echo "  $VALUE"
+else
+  echo "$BATTERY_ICON $VALUE"
+fi
