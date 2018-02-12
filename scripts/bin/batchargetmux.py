ba4e41c5b2a9f67e8989858c83019d96a1f338ed
Abin Simon <abinsimon10@gmail.com>
2018-02-12 10:20:19 +0530

    cleanup

--- scripts/bin/batchargetmux.py
+++ scripts/bin/batchargetmux.py
@@ -2,6 +2,7 @@
 # coding=UTF-8
 
 import math, subprocess
+import sys
 
 color_green = '%{[32m%}'
 color_yellow = '%{[1;33m%}'
@@ -24,29 +25,28 @@ try:
     # Output
     total_slots, slots = 10, []
     filled = int(math.ceil(charge_threshold * (total_slots / 10.0))) * u'▸'
-    filled_string = int((charge_threshold * (total_slots)/10))
+    filled_string = int((charge_threshold * (total_slots) / 10))
     empty = (total_slots - len(filled)) * u'▹'
 
     # out = (filled + empty).encode('utf-8')
     out = str(filled_string)
 
     color_out = (
-            color_green if len(filled) > 6
-            else color_yellow if len(filled) > 4
-            else color_red
-            )
+        color_green if len(filled) > 6
+        else color_yellow if len(filled) > 4
+        else color_red
+    )
 except:
     cur = open('/sys/class/power_supply/BAT0/charge_now', 'r').read()
     full = open('/sys/class/power_supply/BAT0/charge_full', 'r').read()
-    out = str(int(float(cur)/float(full)*100))
+    out = str(int(float(cur) / float(full) * 100))
 
     color_out = (
-            color_green if int(out) > 60
-            else color_yellow if int(out) > 40
-            else color_red
-            )
+        color_green if int(out) > 60
+        else color_yellow if int(out) > 40
+        else color_red
+    )
 
-import sys
 
 out = '±' + out
 sys.stdout.write(out)
