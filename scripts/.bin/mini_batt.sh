83a42ba55f6152a34a92557dce4f0233f83ad84f
Abin Simon <abinsimon10@gmail.com>
2018-03-28 14:40:08 +0530

    [multi] add icons

--- scripts/.bin/mini_batt.sh
+++ scripts/.bin/mini_batt.sh
@@ -1,18 +1,19 @@
 VALUE=$(pmset -g batt | grep InternalBattery | awk '{print $3}' | sed 's:..$::g')
 SOURCE=$(pmset -g batt | grep 'Now drawing from' | awk '{print $4}' | sed 's:^.::g')
 #   i      
-BATTERY_ICON=" "
-if [ $VALUE -lt 20 ];then
-  BATTERY_ICON=" "
-elif [ $VALUE -lt 40 ];then
-  BATTERY_ICON=" "
-elif [ $VALUE -lt 70 ];then
-  BATTERY_ICON=" "
-elif [ $VALUE -lt 90 ];then
-  BATTERY_ICON=" "
-fi
+# BATTERY_ICON=" "
+# if [ $VALUE -lt 20 ];then
+#   BATTERY_ICON=" "
+# elif [ $VALUE -lt 40 ];then
+#   BATTERY_ICON=" "
+# elif [ $VALUE -lt 70 ];then
+#   BATTERY_ICON=" "
+# elif [ $VALUE -lt 90 ];then
+#   BATTERY_ICON=" "
+# fi
 if [ "$SOURCE" = "AC" ]; then
-  echo "$VALUE"%
+  echo "  $VALUE"
 else
-  echo "$VALUE"%
+  # You can actually use $BATTERY ICON TO SHOW PERCENTAGE
+  echo " $VALUE"
 fi
