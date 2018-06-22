9af8a1b8464bd2fa7bc9c1013fd19f5ad407ccd8
Abin Simon <abinsimon10@gmail.com>
2018-06-22 19:14:57 +0530

    [install] install homebrew first

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,9 @@
+# install homebrew
+if test ! "$( which brew )"; then
+    echo "Installing homebrew"
+    ruby -e "$( curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install )"
+fi
+
 # brew
 brew install tmux
 brew install tree
