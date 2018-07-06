c151e5332ae89bcc112bd5234a66eea1e290c5b0
Abin Simon <abinsimon10@gmail.com>
2018-07-06 16:49:27 +0530

    [hammerspoon] Disable arrow keys
    Wanna buy http://www.wasdkeyboards.com/index.php/products/mechanical-keyboard/wasd-vp3-61-key-custom-mechanical-keyboard.html

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -14,19 +14,31 @@ end))
 hs.hotkey.bind({'alt'}, 'space', (function()
   hs.eventtap.keyStroke({"ctrl"}, "Left")
 end))
+hs.hotkey.bind({}, 'Up', (function()
+  hs.eventtap.keyStroke({},"")
+end))
+hs.hotkey.bind({}, 'Down', (function()
+  hs.eventtap.keyStroke({},"")
+end))
+hs.hotkey.bind({}, 'Left', (function()
+  hs.eventtap.keyStroke({},"")
+end))
+hs.hotkey.bind({}, 'Right', (function()
+  hs.eventtap.keyStroke({},"")
+end))
 
 
 -- Auto-reload config on change
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
     end
-    if doReload then
-        hs.reload()
-    end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 hs.notify.new({title="Hammerspoon", informativeText="Hammerspoon config reloaded!"}):send()
