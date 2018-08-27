e0cd4a33dc5a11d276851b7c782ffb1eeb57d207
Abin Simon <abinsimon10@gmail.com>
2018-08-27 11:01:40 +0530

    [update] make update a script

---
+++ install/update.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# Update homebrew
+brew update
+brew upgrade
+
+# update oh-my-zsh
+upgrade_oh_my_zsh
+
+# update vim plugins
+nvim +PlugUpgrade +PlugUpdate +qall
+
+# update mail
+offlineimap
+
+# update rust build tools
+rustup update stable
+rustup update nightly
