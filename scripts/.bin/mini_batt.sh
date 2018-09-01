d27a0aeb32631ca66b961204dc15fca7608c656c
Abin Simon <abinsimon10@gmail.com>
2018-09-01 17:50:46 +0530

    [tmux] add a red background when battery is lower than 10%

--- scripts/.bin/mini_batt.sh
+++ scripts/.bin/mini_batt.sh
@@ -1,18 +1,26 @@
 VALUE=$(pmset -g batt | grep InternalBattery | awk '{print $3}' | sed 's:..$::g')
 SOURCE=$(pmset -g batt | grep 'Now drawing from' | awk '{print $4}' | sed 's:^.::g')
+
+# BATTERY_ICON is only available with patched fonts
 #   i      
 BATTERY_ICON=" "
-if [ $VALUE -lt 20 ];then
+if [ $VALUE -lt 10 ];then
+  VALUE="#[bg=red,fg=white] LOW BATTERY | $VALUE% #[bg=default]"
+elif [ $VALUE -lt 20 ];then
   BATTERY_ICON=" "
+  VALUE="$VALUE%"
 elif [ $VALUE -lt 40 ];then
   BATTERY_ICON=" "
+  VALUE="$VALUE%"
 elif [ $VALUE -lt 70 ];then
   BATTERY_ICON=" "
+  VALUE="$VALUE%"
 elif [ $VALUE -lt 90 ];then
   BATTERY_ICON=" "
+  VALUE="$VALUE%"
 fi
 if [ "$SOURCE" = "AC" ]; then
-  echo "$VALUE"%
+  echo "$VALUE"
 else
-  echo "$VALUE"%
+  echo "$VALUE"
 fi
