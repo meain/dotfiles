74d7d1f5dc091a7ad3466c661a0f7acf58eb0c65
Abin Simon <abinsimon10@gmail.com>
2018-09-01 21:35:52 +0530

    [tmux] fix battery icon % for >90

--- scripts/.bin/mini_batt.sh
+++ scripts/.bin/mini_batt.sh
@@ -4,19 +4,21 @@ SOURCE=$(pmset -g batt | grep 'Now drawing from' | awk '{print $4}' | sed 's:^.:
 # BATTERY_ICON is only available with patched fonts
 #   i      
 BATTERY_ICON=" "
-if [ $VALUE -lt 10 ];then
+if [ "$VALUE" -lt 10 ];then
   VALUE="#[bg=red,fg=white] LOW BATTERY | $VALUE% #[bg=default]"
-elif [ $VALUE -lt 20 ];then
+elif [ "$VALUE" -lt 20 ];then
   BATTERY_ICON=" "
   VALUE="$VALUE%"
-elif [ $VALUE -lt 40 ];then
+elif [ "$VALUE" -lt 40 ];then
   BATTERY_ICON=" "
   VALUE="$VALUE%"
-elif [ $VALUE -lt 70 ];then
+elif [ "$VALUE" -lt 70 ];then
   BATTERY_ICON=" "
   VALUE="$VALUE%"
-elif [ $VALUE -lt 90 ];then
+elif [ "$VALUE" -lt 90 ];then
   BATTERY_ICON=" "
+  VALUE="$VALUE%"
+else
   VALUE="$VALUE%"
 fi
 if [ "$SOURCE" = "AC" ]; then
