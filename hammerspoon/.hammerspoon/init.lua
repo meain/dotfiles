6bdb7ba04b5799a99157e02fc9b0af22bd07ff3e
Abin Simon <abinsimon10@gmail.com>
2018-10-12 15:24:13 +0530

    [hammerspoon] ctrl+cmd s to switch screens

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -9,6 +9,30 @@ hs.window.animationDuration = 0
 
 -- Simle key remaps
 
+hs.hotkey.bind({"ctrl", "cmd"}, "s", function ()
+  focusScreen(hs.window.focusedWindow():screen():next())
+end)
+
+--Predicate that checks if a window belongs to a screen
+function isInScreen(screen, win)
+  return win:screen() == screen
+end
+
+function focusScreen(screen)
+  --Get windows within screen, ordered from front to back.
+  --If no windows exist, bring focus to desktop. Otherwise, set focus on
+  --front-most application window.
+  local windows = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+  windowToFocus:focus()
+
+  -- Move mouse to center of screen
+  local pt = hs.geometry.rectMidPoint(screen:fullFrame())
+  hs.mouse.setAbsolutePosition(pt)
+end
+
 -- Auto-reload config on change
 function reloadConfig(files)
   doReload = false
