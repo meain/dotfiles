6641102d00bbd21febcfc25c5e50a61109ed6098
Abin Simon <abinsimon10@gmail.com>
2019-02-14 16:51:36 +0530

    [hammerspoon] split up screen switch command

---
+++ hammerspoon/.hammerspoon/switchscreen.lua
@@ -0,0 +1,23 @@
+local switchscreen = {}
+
+--Predicate that checks if a window belongs to a screen
+function isInScreen(screen, win)
+  return win:screen() == screen
+end
+
+function switchscreen.focusScreen(screen)
+  --Get windows within screen, ordered from front to back.
+  --If no windows exist, bring focus to desktop. Otherwise, set focus on
+  --front-most application window.
+  local windows = hs.fnutils.filter(
+      hs.window.orderedWindows(),
+      hs.fnutils.partial(isInScreen, screen))
+  local windowToFocus = #windows > 0 and windows[1] or hs.window.desktop()
+  windowToFocus:focus()
+
+  -- Move mouse to center of screen
+  local pt = hs.geometry.rectMidPoint(screen:fullFrame())
+  hs.mouse.setAbsolutePosition(pt)
+end
+
+return switchscreen
