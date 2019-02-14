7ea6fdf27132a95408e9a47cf8e30f5a01d6514a
Abin Simon <abinsimon10@gmail.com>
2019-02-14 19:42:37 +0530

    [hammerspoon] no need for number in jumpcut thingy

--- hammerspoon/.hammerspoon/jumpcut.lua
+++ hammerspoon/.hammerspoon/jumpcut.lua
@@ -19,11 +19,7 @@ local clipboard_history = settings.get("so.victor.hs.jumpcut") or {} --If no his
 
 -- Append a history counter to the menu
 function setTitle()
-  if (#clipboard_history == 0) then
     jumpcut:setTitle("✂") -- Unicode magic
-    else
-      jumpcut:setTitle("✂ ("..#clipboard_history..")") -- updates the menu counter
-  end
 end
 
 function putOnPaste(string,key)
