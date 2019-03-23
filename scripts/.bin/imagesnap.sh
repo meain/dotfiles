26ee2dc658a27a9757896e14d7056534007e42d1
Abin Simon <abinsimon10@gmail.com>
2019-03-23 14:46:56 +0530

    [scripts] move imagescript to bin

---
+++ scripts/.bin/imagesnap.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+DATE=$(date "+%Y-%m-%d_%H-%M-%S")
+imagesnap -w 2.00 "$HOME/.loginimages/$DATE.jpg"
--- scripts/.imagescript/imagesnap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-DATE=$(date "+%Y-%m-%d_%H-%M-%S")
-imagesnap -w 2.00 "/Users/abinsimon/.loginimages/$DATE.jpg"
