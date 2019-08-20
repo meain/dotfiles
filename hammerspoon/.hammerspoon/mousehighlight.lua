9cfbe03c036e200ca468c15726d3c8f47d6c2323
Abin Simon <abinsimon10@gmail.com>
2019-08-20 11:44:32 +0530

    [hammerspoon] visual changes to mousehighlighht

--- hammerspoon/.hammerspoon/mousehighlight.lua
+++ hammerspoon/.hammerspoon/mousehighlight.lua
@@ -14,14 +14,13 @@ function mouseHighlight()
     -- Get the current co-ordinates of the mouse pointer
     mousepoint = hs.mouse.getAbsolutePosition ()
     -- Prepare a big red circle around the mouse pointer
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
-    mouseCircle:setStrokeColor({["red"]=1,["blue"]=0,["green"]=0,["alpha"]=1})
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-4000, mousepoint.y-4000, 8000, 8000))
+    mouseCircle:setStrokeColor({["red"]=0.25,["blue"]=0.69,["green"]=0.95,["alpha"]=0.5})
     mouseCircle:setFill(false)
-    mouseCircle:setStrokeWidth(5)
+    mouseCircle:setStrokeWidth(3980)
     mouseCircle:show()
 
-    -- Set a timer to delete the circle after 3 seconds
-    mouseCircleTimer = hs.timer.doAfter(3, function() mouseCircle:delete() end)
+    mouseCircleTimer = hs.timer.doAfter(1, function() mouseCircle:delete() end)
 end
 
-hs.hotkey.bind({"cmd","alt","ctrl"}, "D", mouseHighlight)
+hs.hotkey.bind({"alt","shift"}, "M", mouseHighlight)
