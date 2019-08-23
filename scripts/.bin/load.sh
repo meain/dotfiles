5b951903d107f2ca9c74a5bfbfe03e26b9c846b9
Abin Simon <abinsimon10@gmail.com>
2019-08-23 18:42:37 +0530

    [scripts] do not need a seperatet `(HIGH)` indicator for load

--- scripts/.bin/load.sh
+++ scripts/.bin/load.sh
@@ -2,7 +2,7 @@
 LOAD=$(ps -A -o %cpu | awk '{s+=$1} END {print s}' | sed  's/\..*//')
 if [ $LOAD -ge 150 ];
 then
-  echo "#[fg=red]$LOAD% (HIGH)#[fg=default]"
+  echo "#[fg=red]$LOAD%#[fg=default]"
 else
   echo "$LOAD%"
 fi
