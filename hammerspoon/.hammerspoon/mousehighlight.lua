8bcfce654c6a4b6cde153c48743fbcd9ba03f513
Abin Simon <abinsimon10@gmail.com>
2019-08-30 17:35:06 +0530

    [hammerspoon] same mousehighlight values
    It was having perf issues rendering that huge thing

--- hammerspoon/.hammerspoon/mousehighlight.lua
+++ hammerspoon/.hammerspoon/mousehighlight.lua
@@ -14,10 +14,10 @@ function mouseHighlight()
     -- Get the current co-ordinates of the mouse pointer
     mousepoint = hs.mouse.getAbsolutePosition ()
     -- Prepare a big red circle around the mouse pointer
-    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-4000, mousepoint.y-4000, 8000, 8000))
-    mouseCircle:setStrokeColor({["red"]=0.25,["blue"]=0.69,["green"]=0.95,["alpha"]=0.5})
+    mouseCircle = hs.drawing.circle(hs.geometry.rect(mousepoint.x-40, mousepoint.y-40, 80, 80))
+    mouseCircle:setStrokeColor({["red"]=1.0,["blue"]=0.69,["green"]=0.95,["alpha"]=0.75})
     mouseCircle:setFill(false)
-    mouseCircle:setStrokeWidth(3980)
+    mouseCircle:setStrokeWidth(20)
     mouseCircle:show()
 
     mouseCircleTimer = hs.timer.doAfter(1, function() mouseCircle:delete() end)
