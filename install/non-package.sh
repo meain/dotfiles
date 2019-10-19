5db0ab081aab9b9acb2f02177d318ab2ce723bdf
Abin Simon <abinsimon10@gmail.com>
2019-10-19 17:26:20 +0530

    [install] no need for oh-my-zsh

--- install/non-package.sh
+++ install/non-package.sh
@@ -1,7 +1,3 @@
 # vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-# oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
