efb226f9569daf9ec8258c4c595ddeadf1fa59dc
Abin Simon <abinsimon10@gmail.com>
2019-11-07 13:33:16 +0530

    [hammerspoon] it was just too slow to load zsh
    fixed by adding the correct node path directly in the spotify code

--- hammerspoon/.hammerspoon/customshellrun.lua
+++ hammerspoon/.hammerspoon/customshellrun.lua
@@ -4,9 +4,9 @@ local module = {}
 
 module.run = function(script)
     -- no need to specify full path in script, but just too slow
-    local result = hs.execute(script, '/bin/zsh')
+    -- local result = hs.execute(script, '/bin/zsh')
 
-    -- local result = hs.execute(script)
+    local result = hs.execute(script)
     return utils.trim(result)
 end
 
