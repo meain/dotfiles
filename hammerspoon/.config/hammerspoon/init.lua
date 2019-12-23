0d8a9395d2e422cac819d42f302ad29c6eaea18a
Abin Simon <abinsimon10@gmail.com>
2019-12-23 12:30:31 +0530

    [hammerspoon] better email message stripping

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -121,7 +121,7 @@ emailNotify = function(paths, flags)
     return mailListing
   end
 
-  result = customshellrun.run(BIN .. 'notifyableunreadmails')
+  result = customshellrun.run(BIN .. 'notifyableunreadmails | cut -c-120')
   unreadcount = utils.linecount(result)
   if (unreadcount > 0) then
     mailcounter:setTitle(unreadcount)
