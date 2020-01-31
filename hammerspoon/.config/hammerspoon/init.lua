bd17c8d664b3b9a6e1b8464399d14d9b67d9e58c
Abin Simon <abinsimon10@gmail.com>
2020-01-31 18:08:29 +0530

    [hammerspoon] use kitty as scratch terminal

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -151,3 +151,23 @@ hs.hotkey.bind({'ctrl', 'alt', 'shift'}, 'e', function()
   hs.alert('📫 Syncing email')
   customshellrun.run(BIN .. 'mailsync')
 end)
+
+hs.hotkey.bind({"alt"}, ";", function()
+  local app = hs.application.get("kitty")
+
+  if app then
+      if not app:mainWindow() then
+          app:selectMenuItem({"kitty", "New OS window"})
+      elseif app:isFrontmost() then
+          app:hide()
+      else
+          app:activate()
+      end
+  else
+      hs.application.launchOrFocus("kitty")
+      app = hs.application.get("kitty")
+  end
+
+  app:mainWindow():moveToUnit'[80,100,10,50]'
+  app:mainWindow().setShadows(false)
+end)
