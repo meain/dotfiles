f52954b1dfcbdb7a76de806c4db3da0e2f2d87fd
Abin Simon <abinsimon10@gmail.com>
2020-06-23 15:10:41 +0530

    [hammerspoon] fix bug with prev window on focusandback

--- hammerspoon/.config/hammerspoon/focusandback.lua
+++ hammerspoon/.config/hammerspoon/focusandback.lua
@@ -2,14 +2,11 @@ local prev_items = {}
 
 function focusandback(app)
     local toopen = hs.application(app)
-    if prev_items[app] == nil then
-        prev_items[app] = hs.window.focusedWindow()
-    end
-
     if toopen and toopen:isFrontmost() then
         prev_items[app]:focus()
         prev_items[app] = null
     else
+        prev_items[app] = hs.window.focusedWindow()
         hs.application.launchOrFocus(app)
     end
 end
