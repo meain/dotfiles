4f89d4adb799a40ad2719b4faaa24b75ee72bd29
Abin Simon <abinsimon10@gmail.com>
2020-06-24 12:13:37 +0530

    [greasemonkey] initial commit

---
+++ greasemonkey/duckduck-lifeboat/duckduck-lifeboat.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name     duckduck-lifeboat
+// @version  1
+// @grant    none
+// ==/UserScript==
+
+he = document.getElementsByClassName("header__search-wrap")[0];
+sb = document.createElement("a");
+tex = document.createTextNode("useGoogle()");
+sb.setAttribute('style', 'margin-left:10px; color: #ccc;cursor:pointer;')
+sb.appendChild(tex);
+he.appendChild(sb);
+sb.onclick = () => {
+  const searchTerm = document.getElementById("search_form_input").value;
+  if (searchTerm)
+    window.location = `https://www.google.com/search?q=${searchTerm}`;
+};
