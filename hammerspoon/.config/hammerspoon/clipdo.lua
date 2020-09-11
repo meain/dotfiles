485a1ae0ea055aad3bf7c87872c051e5daefb083
Abin Simon <abinsimon10@gmail.com>
2020-09-11 12:32:31 +0530

    [hammerspoon] cleanup clipdo.lua

--- hammerspoon/.config/hammerspoon/clipdo.lua
+++ hammerspoon/.config/hammerspoon/clipdo.lua
@@ -2,15 +2,15 @@ local mod = {}
 
 local pasteboard = require("hs.pasteboard")
 
-local customshellrun = require('customshellrun') 
+local customshellrun = require('customshellrun')
 local utils = require("utils")
 
 -- TODO: move this to 'datafiles' dir and read from there
-doables = {'add-to-gourcer', 'open', 'openorsearch', 'gource', 'tempg'}
+local doables = {'add-to-gourcer', 'open', 'openorsearch', 'gource', 'tempg'}
 
 function mod.clipdo()
-    function formatChoices(choices)
-        formattedChoices = hs.fnutils.imap(choices, function(item)
+    local function formatChoices(choices)
+        local formattedChoices = hs.fnutils.imap(choices, function(item)
             return { ["text"] = utils.trim(item) }
         end)
         return formattedChoices
@@ -19,7 +19,7 @@ function mod.clipdo()
     local link = utils.trim(pasteboard.getContents())
     local chooser = hs.chooser.new(function(chosen)
         hs.alert("🧧 " .. chosen.text .. " '" .. link .. "'")
-        result = customshellrun.run(chosen.text .. " '" .. link .. "'", true)
+        local result = customshellrun.run(chosen.text .. " '" .. link .. "'", true)
         if (result == nil or result == '') then
             hs.alert('-- no output --')
         else
