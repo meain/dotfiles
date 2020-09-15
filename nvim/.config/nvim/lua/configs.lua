23676d2320c8b445408fd887c11c8b64095cc750
Abin Simon <abinsimon10@gmail.com>
2020-09-15 19:55:31 +0530

    [nvim] disable definitions highlight in ts

--- nvim/.config/nvim/lua/configs.lua
+++ nvim/.config/nvim/lua/configs.lua
@@ -3,7 +3,7 @@ require "nvim-treesitter.configs".setup {
   ensure_installed = "all",
   highlight = {enable = true},
   refactor = {
-    highlight_definitions = {enable = true},
+    highlight_definitions = {enable = false},
     highlight_current_scope = {enable = false}
   },
   textobjects = {
