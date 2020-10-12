c25d42ebc72f0a6ae137163ba8ecac1312499eb0
Abin Simon <abinsimon10@gmail.com>
2020-10-12 10:36:08 +0530

    [emacs] improve scratch stuff

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -504,23 +504,14 @@
 
 ;; Quick open scratch buffers
 (use-package scratch
-  :ensure :config
-  (defun meain/scratch-buffer-setup ()
-    "Add contents to `scratch' buffer and name it accordingly."
-    (let* ((mode (format "%s" major-mode))
-	   (string (concat "Scratch buffer for: " mode "\n\n")))
-      (when scratch-buffer
-	(save-excursion
-	  (insert string)
-	  (goto-char (point-min))
-	  (comment-region (point-at-bol)
-			  (point-at-eol)))
-	(forward-line 2))
-      (rename-buffer (concat "*Scratch for " mode "*")
-		     t)))
-  :hook (scratch-create-buffer-hook . meain/scratch-buffer-setup):init
-  (evil-leader/set-key "c" 'scratch))
-
+  :ensure t
+  :config (progn
+	    (defun meain/scratch (&optional alternate)
+	      (interactive "P")
+	      (if alternate
+		  (call-interactively 'scratch)
+		(switch-to-buffer "*scratch*")))
+	    (evil-leader/set-key "c" 'meain/scratch)))
 
 ;; Highlight color codes
 (use-package rainbow-mode
