cc3f247ce258fb88c2230ca6704082f8af69d559
Abin Simon <abinsimon10@gmail.com>
2020-11-24 19:20:18 +0530

    [hammerspoon] use alt+; to open item in non emacs windows

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -349,6 +349,22 @@ hs.hotkey.bind(
     end
 )
 
+-- floating terminal/emacs
+floatingterm = hs.hotkey.bind(
+    {"alt"},
+    ";",
+    function()
+        local current_app = hs.window.focusedWindow()
+        if string.match(current_app:title(), "Emacs") then
+            floatingterm:disable()
+            hs.eventtap.keyStroke({"alt"}, ";")
+            floatingterm:enable()
+        else
+            hs.eventtap.keyStroke({"cmd"}, ";")
+        end
+    end
+)
+
 -- quick window switching
 hs.hotkey.bind(
     {"alt"},
