7d53f76a4ad931bb28440b84c4c904f29adc0e64
Abin Simon <abinsimon10@gmail.com>
2020-12-27 17:35:51 +0530

    [emacs] setup sqls for eglot
    https://www.reddit.com/r/emacs/comments/ijbvwv/eglot_sqls_sql_client/

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -608,6 +608,87 @@
     (add-hook 'eglot-managed-mode-hook
               (lambda ()
                 (eldoc-mode -1)))
+    (add-to-list 'display-buffer-alist
+                 '("\\*sqls\\*"
+                   (display-buffer-reuse-window display-buffer-at-bottom)
+                   (reusable-frames . visible)
+                   (window-height . 0.3)))
+    (defclass eglot-sqls (eglot-lsp-server)
+      ()
+      :documentation "SQL's Language Server")
+    (add-to-list 'eglot-server-programs
+                 '(sql-mode . (eglot-sqls "sqls" "-config" "~/.config/sqls/config.yaml")))
+    (cl-defmethod eglot-execute-command
+      ((server eglot-sqls)
+       (command (eql executeQuery))
+       arguments)
+      "For executeQuery."
+      (let* ((beg (eglot--pos-to-lsp-position (if (use-region-p)
+                                                  (region-beginning)
+                                                (point-min))))
+             (end (eglot--pos-to-lsp-position (if (use-region-p)
+                                                  (region-end)
+                                                (point-max))))
+             (res (jsonrpc-request server
+                                   :workspace/executeCommand `(:command ,(format "%s" command)
+                                                                        :arguments ,arguments
+                                                                        :timeout 0.5
+                                                                        :range (:start ,beg :end ,end))))
+             (buffer (generate-new-buffer "*sqls*")))
+        (with-current-buffer buffer
+          (eglot--apply-text-edits `[(:range (:start (:line 0 :character 0)
+                                                     :end (:line 0 :character 0))
+                                             :newText ,res)])
+          (org-mode))
+        (pop-to-buffer buffer)))
+    (cl-defmethod eglot-execute-command
+      ((server eglot-sqls)
+       (_cmd (eql switchDatabase))
+       arguments)
+      "For switchDatabase."
+      (let* ((res (jsonrpc-request server
+                                   :workspace/executeCommand `(:command "showDatabases" :arguments ,arguments
+                                                                        :timeout 0.5)))
+             (menu-items (split-string res "\n"))
+             (menu `("Eglot code actions:"
+                     ("dummy" ,@menu-items)))
+             (db (if (listp last-nonmenu-event)
+                     (x-popup-menu last-nonmenu-event menu)
+                   (completing-read "[eglot] Pick an database: "
+                                    menu-items
+                                    nil
+                                    t
+                                    nil
+                                    nil
+                                    (car menu-items)))))
+        (jsonrpc-request server
+                         :workspace/executeCommand `(:command "switchDatabase"
+                                                              :arguments [,db]:timeout
+                                                              0.5))))
+    (cl-defmethod eglot-execute-command
+      ((server eglot-sqls)
+       (_cmd (eql switchConnections))
+       arguments)
+      "For switchConnections"
+      (let* ((res (jsonrpc-request server
+                                   :workspace/executeCommand `(:command "switchConnections" :arguments ,arguments
+                                                                        :timeout 0.5)))
+             (menu-items (split-string res "\n"))
+             (menu `("Eglot code actions:"
+                     ("dummy" ,@menu-items)))
+             (db (if (listp last-nonmenu-event)
+                     (x-popup-menu last-nonmenu-event menu)
+                   (completing-read "[eglot] Pick an connection "
+                                    menu-items
+                                    nil
+                                    t
+                                    nil
+                                    nil
+                                    (car menu-items)))))
+        (jsonrpc-request server
+                         :workspace/executeCommand `(:command "switchConnections"
+                                                              :arguments [,db]:timeout
+                                                              0.5))))
     (define-key evil-normal-state-map (kbd "K") 'eldoc-print-current-symbol-info)
     (define-key evil-normal-state-map (kbd "g k") 'eldoc-print-current-symbol-info)
     (define-key evil-normal-state-map (kbd "g d") 'xref-find-definitions)
