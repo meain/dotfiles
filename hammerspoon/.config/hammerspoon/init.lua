48f7e7b728057542165c2d2920dae8c0941b495a
Abin Simon <abinsimon10@gmail.com>
2021-01-29 18:24:25 +0530

    [hammerspoon] taskwarrior canvas uses tiny as command

--- hammerspoon/.config/hammerspoon/init.lua
+++ hammerspoon/.config/hammerspoon/init.lua
@@ -564,7 +564,7 @@ hs.hotkey.bind(
 )
 
 local canvas = nil
-local canvascommand = "/usr/local/bin/task tot | tail -n+4 | sed '$ d'"
+local canvascommand = "/usr/local/bin/task tiny | tail -n+4 | sed '$ d'"
 function showOutputInCanvas()
     local hcaltitlecolor = {red = 1, blue = 1, green = 1, alpha = 0.5}
     local cscreen = hs.screen.mainScreen()
