95180219a41f35d9b2b85891b335e13fa1683025
Abin Simon <abinsimon10@gmail.com>
2021-03-12 16:34:29 +0530

    [emacs] remove DEBUG from hl-todo keyword list

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1170,7 +1170,6 @@ Pass ORIGINAL and ALTERNATE options."
   :config (progn
             (setq hl-todo-keyword-faces '(("TODO" . "#FF0000")
                                           ("FIXME" . "#FF0000")
-                                          ("DEBUG" . "#A020F0")
                                           ("GOTCHA" . "#FF4500")
                                           ("STUB" . "#1E90FF")))
             (global-hl-todo-mode)))
