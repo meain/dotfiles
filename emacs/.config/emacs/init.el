d5abe8190f45fbf0219d2256320c24f88efc4394
Abin Simon <abinsimon10@gmail.com>
2021-04-24 19:38:45 +0530

    [emacs] auto insert mail signature

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1449,6 +1449,8 @@ Pass ORIGINAL and ALTERNATE options."
                                                 :sort-order newest-first))))))
 
 ;; sending emails
+(setq mail-signature t)
+(setq mail-signature-file "~/.config/datafiles/mailsignature")
 (setq message-kill-buffer-on-exit t) ; kill buffer after sending mail
 (setq mail-specify-envelope-from t) ; Settings to work with msmtp
 (setq message-sendmail-envelope-from 'header)
