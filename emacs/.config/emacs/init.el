5682c0054283a51d57df93f60d5b26f7ce3fe27c
Abin Simon <abinsimon10@gmail.com>
2021-09-26 09:52:00 +0530

    [emacs] use-package: enable verbose logging of package load

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -24,6 +24,7 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+(setq use-package-verbose t)
 (straight-use-package 'use-package)
 (setq use-package-always-demand (getenv "LOAD_FULL_EMACS"))
 
