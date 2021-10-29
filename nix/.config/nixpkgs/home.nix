d0e7f6054d42c5ab51df4410b695e3f84cd7f934
Abin Simon <mail@meain.io>
2021-10-29 09:34:52 +0530

    [nix] update home-manager

--- nix/.config/nixpkgs/home.nix
+++ nix/.config/nixpkgs/home.nix
@@ -1,191 +1,261 @@
-# home-manager switch
-{ config, pkgs, ... }:
-
+{ lib, config, pkgs, ... }:
+let
+personal = import (builtins.fetchTarball "https://github.com/meain/nix-channel/archive/c146160cd9520808f9ca6e749a6d9e8fc4742de1.tar.gz"){};
+in
 {
-  # home.stateVersion = "21.05";
-
-  # home.username = "meain";
-  # home.homeDirectory = "/Users/meain";
-
+  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-  # nixpkgs.config.allowUnfree = true;
 
-  home.packages = with pkgs; [
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "meain";
+  home.homeDirectory = "/home/meain";
+
+  home.packages = [
     # core utils
-    ripgrep
-    fd
-    jq
-    git
-    fzf
-    tree
-    cmake
-    curl
-    zsh
-    coreutils
+    pkgs.ripgrep
+    pkgs.fd
+    pkgs.jq
+    pkgs.git
+    pkgs.fzf
+    pkgs.tree
+    pkgs.cmake
+    pkgs.curl
+    pkgs.zsh
+    pkgs.coreutils
 
     # packages
-    notmuch # mail indexer
-    isync # mail syncronize with upstrem
-    htop # process monitor
-    mpc_cli # remote for mpd
-    mpd # music player
-    axel # download manager
-    pandoc # convert document between different formats
-    xsv # view/manage csv from terminal
-    parallel # exec things in parallel
-    fortune # give me a fortune
-    lf # better ranger alternative
-    diff-so-fancy # really good diff
-    icdiff # simple colorfull diff replacement
-    ctags # code tag stuff
-    pass # password management
-    sshfs # mount vm as fs using ssh
-    stow # symlink management
-    git-absorb # automatic git commit --fixup
-    git-crypt # encrypt git stuff
-    wget # get stuff from internet
-    tmux # terminal multiplexer
-    aspell # spell checker
-    # mpvc  # mpv remote control  # not available on macos
-    msmtp # smtp client
-    # android-tools # adb and friends  # not available for macos
-    restic # backup
-    ledger # double entry accounting
-    dasel # jq but more versatile
-    # haskellPackages.kmonad # keyboard remapping
-    mosh
+    pkgs.notmuch # mail indexer
+    pkgs.isync # mail syncronize with upstrem
+    pkgs.htop # process monitor
+    pkgs.mpc_cli # remote for mpd
+    pkgs.mpd # music player
+    pkgs.axel # download manager
+    pkgs.pandoc # convert document between different formats
+    pkgs.xsv # view/manage csv from terminal
+    pkgs.parallel # exec things in parallel
+    pkgs.fortune # give me a fortune
+    pkgs.lf # better ranger alternative
+    pkgs.diff-so-fancy # really good diff
+    pkgs.icdiff # simple colorfull diff replacement
+    pkgs.ctags # code tag stuff
+    pkgs.pass # password management
+    pkgs.sshfs # mount vm as fs using ssh
+    pkgs.stow # symlink management
+    pkgs.git-absorb # automatic git commit --fixup
+    pkgs.git-crypt # encrypt git stuff
+    pkgs.wget # get stuff from internet
+    pkgs.tmux # terminal multiplexer
+    pkgs.aspell # spell checker
+    # pkgs.mpvc  # mpv remote control  # not available on macos
+    pkgs.msmtp # smtp client
+    # pkgs.android-tools # adb and friends  # not available for macos
+    pkgs.restic # backup
+    pkgs.ledger # double entry accounting
+    pkgs.dasel # jq but more versatile
+    # pkgs.haskellPackages.kmonad # keyboard remapping
+    pkgs.mosh
 
     # aspell dicts
-    aspellDicts.en
+    pkgs.aspellDicts.en
 
     # apps
-    kubectl # kubernetes cli
-    kubernetes-helm # helm cli
-    youtube-dl # download youtube videos
-    chafa # show images in terminal using half blocks
-    hub # Github integration for git
-    lynx # terminal web browser
-    imagemagick # image manip cli
-    ffmpeg # video manip cli
-    gnuplot # plotting
-    ddgr # search ddg from terminal
-    dasht # terminal docs
+    pkgs.kubectl # kubernetes cli
+    pkgs.kubernetes-helm # helm cli
+    pkgs.youtube-dl # download youtube videos
+    pkgs.chafa # show images in terminal using half blocks
+    pkgs.hub # Github integration for git
+    pkgs.lynx # terminal web browser
+    pkgs.imagemagick # image manip cli
+    pkgs.ffmpeg # video manip cli
+    pkgs.gnuplot # plotting
+    pkgs.ddgr # search ddg from terminal
+    pkgs.dasht # terminal docs
     # taskwarrior # task management
-    todo-txt-cli # todo management
-    ts # task spooler
-    pstree # view process tree
-    jiq # interactive jiq
-    tldr # simpler man pages
-    silicon # create pretty code screenshots
-    transmission # torrent stuff
-    kube-prompt # interactive kubernetes cli
-    cowsay # useless stuff
-    podman # pod manager
+    pkgs.todo-txt-cli # todo management
+    pkgs.pkgs.ts # task spooler
+    pkgs.pkgs.pstree # view process tree
+    pkgs.pkgs.jiq # interactive jiq
+    pkgs.pkgs.tldr # simpler man pages
+    pkgs.pkgs.silicon # create pretty code screenshots
+    pkgs.pkgs.transmission # torrent stuff
+    pkgs.pkgs.kube-prompt # interactive kubernetes cli
+    pkgs.pkgs.cowsay # useless stuff
+    pkgs.podman # pod manager
 
     # programming
-    gist # create gist
-    hey # http load generator
+    pkgs.gist # create gist
+    pkgs.hey # http load generator
 
     # programming-shell
-    shellcheck # shell checker
-    shfmt # shell code format
-    nodePackages.bash-language-server # bash language-server
+    pkgs.shellcheck # shell checker
+    pkgs.shfmt # shell code format
+    pkgs.nodePackages.bash-language-server # bash language-server
 
     # programming-go
-    go # go programming language
-    go-langserver # go language-server
-    goimports # go formatter
-    golangci-lint # all kinds of linters for go
+    pkgs.go # go programming language
+    pkgs.go-langserver # go language-server
+    pkgs.goimports # go formatter
+    pkgs.golangci-lint # all kinds of linters for go
 
     # programming-web
-    html-tidy # html formatter
-    nodejs # nodejs
-    nodePackages.neovim # neovim package for js support
-    nodePackages.fixjson # much better json formatter
-    nodePackages.stylelint # css linter
-    nodePackages.prettier # formatting for web stuff
-    nodePackages.pnpm # package management
-    nodePackages.typescript # typescript
-    nodePackages.vscode-css-languageserver-bin # css languageserver
-    nodePackages.javascript-typescript-langserver # javascript langserver
+    pkgs.html-tidy # html formatter
+    pkgs.nodejs # nodejs
+    pkgs.nodePackages.neovim # neovim package for js support
+    pkgs.nodePackages.fixjson # much better json formatter
+    pkgs.nodePackages.stylelint # css linter
+    pkgs.nodePackages.prettier # formatting for web stuff
+    pkgs.nodePackages.pnpm # package management
+    pkgs.nodePackages.typescript # typescript
+    pkgs.nodePackages.vscode-css-languageserver-bin # css languageserver
+    pkgs.nodePackages.javascript-typescript-langserver # javascript langserver
 
     # programming-nix
-    rnix-lsp # nix language server
-    nixpkgs-fmt # nix formater
+    pkgs.rnix-lsp # nix language server
+    pkgs.nixpkgs-fmt # nix formater
 
     # programming-python
-    python39 # python language
-    poetry # better package manager
-    black # python code formatter
-    python39Packages.flake8 # linter
-    python39Packages.ipdb # interactive debugging
-    python39Packages.pynvim # neovim python support
-    python39Packages.pycodestyle # code style check
-    python39Packages.pydocstyle # doc style check
-    python39Packages.requests # http lib for quick stuff
-    python39Packages.virtualenv # virtual envs
-    python39Packages.bandit # analyze code for security issues
-    python39Packages.mypy # check types in code
-    python39Packages.isort # fix sort order
-    python39Packages.pygments # generic syntax highlight
-    # python38Packages.python-language-server # python lsp (using below one as tests are failing)
-    (python38Packages.python-language-server.overridePythonAttrs (oldAttrs: { checkPhase = ""; checkInputs = []; }))
+    pkgs.python39 # python language
+    pkgs.poetry # better package manager
+    pkgs.black # python code formatter
+    pkgs.python39Packages.flake8 # linter
+    pkgs.python39Packages.ipdb # interactive debugging
+    pkgs.python39Packages.pynvim # neovim python support
+    pkgs.python39Packages.pycodestyle # code style check
+    pkgs.python39Packages.pydocstyle # doc style check
+    pkgs.python39Packages.requests # http lib for quick stuff
+    pkgs.python39Packages.virtualenv # virtual envs
+    pkgs.python39Packages.bandit # analyze code for security issues
+    pkgs.python39Packages.mypy # check types in code
+    pkgs.python39Packages.isort # fix sort order
+    pkgs.python39Packages.pygments # generic syntax highlight
+    # pkgs.python38Packages.python-language-server # python lsp (using below one as tests are failing)
+    (pkgs.python38Packages.python-language-server.overridePythonAttrs (oldAttrs: { checkPhase = ""; checkInputs = []; }))
 
     # programming-rust
-    rustc # compiler
-    cargo # package manager
-    rustfmt # formatter
-    clippy # the useful clippy
-    rust-analyzer # lsp for rust
-    cargo-edit # dep management
-    cargo-bloat # find big chunks
-    cargo-udeps # find unnecessary deps
-    cargo-release # for releasing packages
+    pkgs.rustc # compiler
+    pkgs.cargo # package manager
+    pkgs.rustfmt # formatter
+    pkgs.clippy # the useful clippy
+    pkgs.rust-analyzer # lsp for rust
+    pkgs.cargo-edit # dep management
+    pkgs.cargo-bloat # find big chunks
+    pkgs.cargo-udeps # find unnecessary deps
+    pkgs.cargo-release # for releasing packages
     # cargo-watch # continuously run cargo check
 
     # programming-other
-    nodePackages.yaml-language-server
-    nodePackages.vscode-json-languageserver
+    pkgs.nodePackages.yaml-language-server
+    pkgs.nodePackages.vscode-json-languageserver
 
     # gui
-    # mpv # audio/video player
-    alacritty # terminal emulator
+    pkgs.mpv # audio/video player
+    pkgs.alacritty # terminal emulator
 
     # others
-    redis # key value db
-    postgresql_13 # postgres 13 (postgresql is at 11)
-    # mongodb # document db
-    sqlite # better db
-    # minikube # mini kubernetes
-    awscli # manage aws
-    google-cloud-sdk # manage google cloud
+    pkgs.redis # key value db
+    pkgs.postgresql_13 # postgres 13 (postgresql is at 11)
+    # pkgs.mongodb # document db
+    pkgs.sqlite # better db
+    # pkgs.minikube # mini kubernetes
+    pkgs.awscli # manage aws
+    pkgs.google-cloud-sdk # manage google cloud
 
     # optional
-    # ncdu # disk usage viewer tui
-    # bat # cat with syntax highlight
-    # surfraw # search web
-    # httpie # prettier curl for debugging
-    # qrencode # encode data as qr from cli
-    # w3m # terminal web browser
-    # scim # excel for terminal
-    # jrnl # journaling
-    # figlet # make big text
-    # gource # source tree visualisation
-    # tig # tui git interface
-    # lazygit # tui git interface
-    # lazydocker # tui docker interface
-    # ncmpcpp # mpd tui client
-    # tokei # count lines of code
-    # navi # interactive cli launcher
-    # googler # search google from terminal
-    # cmatrix # matrix thingy in shell
-    # graphviz # draw graphs with code
-    # pgcli # fancier postgres cli
-    trivy # docker vulnerability scanner
-    act # github ci locally
+    # pkgs.ncdu # disk usage viewer tui
+    # pkgs.bat # cat with syntax highlight
+    # pkgs.surfraw # search web
+    # pkgs.httpie # prettier curl for debugging
+    # pkgs.qrencode # encode data as qr from cli
+    # pkgs.w3m # terminal web browser
+    # pkgs.scim # excel for terminal
+    # pkgs.jrnl # journaling
+    # pkgs.figlet # make big text
+    # pkgs.gource # source tree visualisation
+    # pkgs.tig # tui git interface
+    # pkgs.lazygit # tui git interface
+    # pkgs.lazydocker # tui docker interface
+    # pkgs.ncmpcpp # mpd tui client
+    # pkgs.tokei # count lines of code
+    # pkgs.navi # interactive cli launcher
+    # pkgs.googler # search google from terminal
+    # pkgs.cmatrix # matrix thingy in shell
+    # pkgs.graphviz # draw graphs with code
+    # pkgs.pgcli # fancier postgres cli
+    pkgs.trivy # docker vulnerability scanner
+    pkgs.act # github ci locally
 
     # tryout
-    # gforth # gnu forth interpreter
-    # groff # gnu troff
+    # pkgs.gforth # gnu forth interpreter
+    # pkgs.groff # gnu troff
+
+    # linux-specific
+    pkgs.victor-mono
+    pkgs.htop
+    pkgs.emacs
+    pkgs.diff-so-fancy
+    personal.traffic
   ];
+
+  dconf.settings = {
+    "org/gnome/nautilus/preferences" = {
+      default-folder-viewer="icon-view";
+    };
+    "org/gnome/nautilus/icon-view" = {
+      default-zoom-level="small";
+    };
+
+    "org/gnome/shell/keybindings" = {
+      toggle-overview=["<Super>Space"];
+    };
+
+    "org/gnome/shell/extensions/dash-to-panel" = {
+      appicon-margin=0;
+      appicon-padding=3;
+      available-monitors=[0];
+      dot-position="BOTTOM";
+      hotkeys-overlay-combo="TEMPORARILY";
+      leftbox-padding=-1;
+      panel-anchors="{\"0\":\"MIDDLE\"}";
+      panel-lengths="{\"0\":100}";
+      panel-positions="{\"0\":\"TOP\"}";
+      panel-sizes="{\"0\":24}";
+      status-icon-padding=-1;
+      tray-padding=-1;
+      window-preview-title-position="TOP";
+    };
+
+    "org/gnome/desktop/interface" = {
+      clock-format = "12h";
+    };
+    "org/gtk/settings/file-chooser" = {
+      clock-format = "12h";
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      # close = ["<Super>w"];
+      switch-to-workspace-left = ["<Alt>u"];
+      switch-to-workspace-right = ["<Alt>i"];
+      move-to-workspace-left = ["<Alt><Shift>u"];
+      move-to-workspace-right = ["<Alt><Shift>i"];
+    };
+  };
+
+  systemd.user.services.kmonad = {
+    Unit = {
+      Description="Kmonadddddd";
+    };
+    # description="Kmonad keyboard remapping thingy";
+    # enable=true;
+    serviceConfig = {
+      ExecStart="${personal.kmonad}/bin/kmonad /home/meain/.config/kmonad/master.kbd";
+    };
+  };
+
+# This value determines the Home Manager release that your # configuration is compatible with. This helps avoid breakage # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.05";
 }
