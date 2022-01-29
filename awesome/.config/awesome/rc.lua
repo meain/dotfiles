ade63cf85630ff814b1849872d3892f236afbf18
Abin Simon <mail@meain.io>
2022-01-29 21:14:08 +0530

    [awesome] add titlebar when in floating mode

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -464,7 +464,19 @@ clientkeys =
         end,
         {description = "toggle fullscreen", group = "client"}
     ),
-    awful.key({modkey}, "f", awful.client.floating.toggle, {description = "toggle floating", group = "client"}),
+    awful.key(
+        {modkey},
+        "f",
+        function(c)
+            awful.client.floating.toggle()
+            if (awful.client.floating.get() == true) then
+                awful.titlebar.show(c)
+            else
+                awful.titlebar.hide(c)
+            end
+        end,
+        {description = "toggle floating", group = "client"}
+    ),
     awful.key(
         {modkey, "Shift"},
         "Return",
@@ -687,4 +699,59 @@ client.connect_signal(
         c.border_color = beautiful.border_normal
     end
 )
--- }}}
+
+-- Add a titlebar if titlebars_enabled is set to true in the rules.
+client.connect_signal(
+    "request::titlebars",
+    function(c)
+        -- buttons for the titlebar
+        local buttons =
+            gears.table.join(
+            awful.button(
+                {},
+                1,
+                function()
+                    c:emit_signal("request::activate", "titlebar", {raise = true})
+                    awful.mouse.client.move(c)
+                end
+            ),
+            awful.button(
+                {},
+                3,
+                function()
+                    c:emit_signal("request::activate", "titlebar", {raise = true})
+                    awful.mouse.client.resize(c)
+                end
+            )
+        )
+
+        awful.titlebar(c):setup {
+            {
+                -- Left
+                awful.titlebar.widget.iconwidget(c),
+                buttons = buttons,
+                layout = wibox.layout.fixed.horizontal
+            },
+            {
+                -- Middle
+                {
+                    -- Title
+                    align = "left",
+                    widget = awful.titlebar.widget.titlewidget(c)
+                },
+                buttons = buttons,
+                layout = wibox.layout.flex.horizontal
+            },
+            {
+                -- Right
+                awful.titlebar.widget.floatingbutton(c),
+                awful.titlebar.widget.maximizedbutton(c),
+                awful.titlebar.widget.stickybutton(c),
+                awful.titlebar.widget.ontopbutton(c),
+                awful.titlebar.widget.closebutton(c),
+                layout = wibox.layout.fixed.horizontal()
+            },
+            layout = wibox.layout.align.horizontal
+        }
+    end
+)
