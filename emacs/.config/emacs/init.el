142386a53c9b1a012f850f1d6449d6e2feda1cd5
Abin Simon <mail@meain.io>
2022-07-29 09:19:34 +0530

    [emacs] update some aas snippets

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -586,7 +586,8 @@ Pass ORIGINAL and ALTERNATE options."
                                                 (_ (meain/go-default-returns return-node-text errformat)))))))))
   (aas-set-snippets 'global
     ";--" "—"
-    ";->" "→"
+    ";>>" "→"
+    ";ghm" "[meain](https://github.com/meain)"
     ";isodate" (lambda () (interactive) (insert (format-time-string "%a, %d %b %Y %T %z")))
     ";date" (lambda () (interactive) (insert (format-time-string "%a %b %d %Y")))
     ";sdate" (lambda () (interactive) (insert (format-time-string "%d %b %Y")))
