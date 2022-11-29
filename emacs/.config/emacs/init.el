17a5561863e80c1ac12575d2d3ee4a27c1d29be0
Abin Simon <mail@meain.io>
2022-11-29 09:44:47 +0530

    [emacs] add a better header for init.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -1,7 +1,45 @@
 ;;; init -- meain's Emacs config -*- lexical-binding: t -*-
 
 ;;; Commentary:
-;; Well, this is a vimmer's Emacs config.  Nothing fancy though.
+;;                    ____,
+;;                   /.---|
+;;                   `    |     ___
+;;                       (=\.  /-. \
+;;                        |\/\_|"|  |
+;;                        |_\ |;-|  ;
+;;                        | / \| |_/ \
+;;                        | )/\/      \
+;;                        | ( '|  \   |
+;;                        |    \_ /   \
+;;                        |    /  \_.--\
+;;                        \    |    (|\`
+;;                         |   |     \
+;;                         |   |      '.
+;;                         |  /         \
+;;                         \  \.__.__.-._)
+;;
+;;
+;; Well, hello there! How are you doing wanderer? Looking for some
+;; lisp goodness?  You might find it here, you might not.  If you do
+;; find what you are looking for here, feel free take them with you,
+;; give them a new life, a new filesystem, a new home.  All I ask of
+;; you is to treat them with love and care.  They have always been
+;; with me, playing along with my musing, catching little typos and
+;; finding little bugs.  They stuck strong to my side even when the
+;; Rust borrow checker came for me.  I'm not gonna lie, there has been
+;; many a times where I have doubted my skills, but they have always
+;; believed in me.
+;;
+;; If these parenthesis could talk, they would have a lot of stories
+;; to tell.  Some good, some bad, some really ugly.  But at the end of
+;; the day, I'm sure they are all happy to be where they are.
+;;
+;; If they give you any trouble, my GitHub issues is always open
+;; unlike the doors of heaven.  They probably won't, these are the
+;; good ones, but God sometimes have different plans, and everyone
+;; gets hit with hard times.
+;;
+;; Good luck!
 
 ;;; Code:
 
