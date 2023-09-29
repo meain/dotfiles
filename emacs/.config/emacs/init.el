8261e3f3509f16e7a619545db1b6aaf7e437f8a7
Abin Simon <mail@meain.io>
2023-09-29 12:10:37 +0530

    [emacs] switch to builtin way of right aligning modeline

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -4720,14 +4720,7 @@ not defined, it will be saved in the `$HOME' directory."
                                                      hima-simple-gray)
                                        ""))
                '(:eval (if (boundp 'keycast-mode-line) keycast-mode-line))
-               '(:eval (propertize " "
-                                   'display
-                                   `((space :align-to (- (+ right right-fringe right-margin)
-                                                         ,(+ 2
-                                                             (+ (string-width (format-mode-line "%p"))
-                                                                (string-width (format-mode-line "%m"))
-                                                                (if (boundp 'org-timer-mode-line-string)
-                                                                    (1+ (string-width org-timer-mode-line-string)) 0)))))))) ;; spacer
+               'mode-line-format-right-align
                '(:eval (if (boundp 'org-timer-mode-line-string) (concat org-timer-mode-line-string " ")))
                (propertize "%p") ;; position in file
                (propertize " %m ")))
