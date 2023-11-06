e48c16ce3d7093f3fe9ecbf7a520dac003fab76d
Abin Simon <mail@meain.io>
2023-11-06 10:04:25 +0530

    [emacs] update elpaca to 0.6

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -52,13 +52,13 @@
   "OpenAI API key.")
 
 ;; Setup elpaca
-(defvar elpaca-installer-version 0.5)
+(defvar elpaca-installer-version 0.6)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
 (defvar elpaca-order '(elpaca :repo "https://github.com/progfolio/elpaca.git"
                               :ref nil
-                              :files (:defaults (:exclude "extensions"))
+                              :files (:defaults "elpaca-test.el" (:exclude "extensions"))
                               :build (:not elpaca--activate-package)))
 (let* ((repo  (expand-file-name "elpaca/" elpaca-repos-directory))
        (build (expand-file-name "elpaca/" elpaca-builds-directory))
