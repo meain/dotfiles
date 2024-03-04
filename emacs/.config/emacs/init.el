079fcc3129c8b89581ef3f74c78acf1882ca50e5
Abin Simon <mail@meain.io>
2024-03-04 10:43:45 +0530

    [emacs] csv-mode: coloring and spacing automatically

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -2819,7 +2819,26 @@ Instead of `default-directory' when calling `ORIG-FN' with `ARGS'."
   :ensure t
   :defer t
   :config
-  (setq csv-align-mode t)
+  ;; https://www.emacswiki.org/emacs/CsvMode
+  (require 'cl)
+  (require 'color)
+  (defun meain/csv-highlight (&optional separator)
+    (interactive (list (when current-prefix-arg (read-char "Separator: "))))
+    (font-lock-mode 1)
+    (let* ((separator (or separator ?\,))
+           (n (count-matches (string separator) (point-at-bol) (point-at-eol)))
+           (colors (loop for i from 0 to 1.0 by (/ 2.0 n)
+                         collect (apply #'color-rgb-to-hex
+                                        (color-hsl-to-rgb i 0.3 0.5)))))
+      (loop for i from 2 to n by 2
+            for c in colors
+            for r = (format "^\\([^%c\n]+%c\\)\\{%d\\}" separator separator i)
+            do (font-lock-add-keywords nil `((,r (1 '(face (:foreground ,c)))))))))
+
+  (add-hook 'csv-mode-hook 'meain/csv-highlight)
+  (add-hook 'csv-mode-hook 'csv-align-mode)
+  (add-hook 'csv-mode-hook '(lambda () (interactive) (toggle-truncate-lines nil)))
+
   (set-face-attribute 'csv-separator-face nil
                       :background "gray100"
                       :foreground "#000000"))
