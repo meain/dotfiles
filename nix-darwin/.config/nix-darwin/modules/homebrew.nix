00dce18ee2f2cf599cf107c569feb1517d84077e
Abin Simon <mail@meain.io>
2024-06-10 12:24:33 +0530

    [nix-darwin] update package, setup auto uninstall

--- nix-darwin/.config/nix-darwin/modules/homebrew.nix
+++ nix-darwin/.config/nix-darwin/modules/homebrew.nix
@@ -2,25 +2,45 @@
 {
   homebrew = {
     enable = true;
+
+    onActivation = {
+      cleanup = "uninstall"; # uninstall any not listed here
+      # upgrade = true; # note idempotent anymore
+    };
+
     global.brewfile = true;
+
     masApps = { }; # from mac app store
     taps = [
-      "jimeh/emacs-builds"
+      "railwaycat/emacsmacport"
+    ];
+    brews = [
+      "emacs-mac"
+      "choose-gui" # dmenu alternative
     ];
     casks = [
-      # "firefox" # installed via nix
-      "hammerspoon"
-      "logseq"
-      "microsoft-teams"
-      "zed"
-      "emacs-app-nightly"
-      "google-chrome"
-      "slack"
-      "hiddenbar"
-      "spotify"
+      "hammerspoon" # make macOS usable
+      "hiddenbar" # hide items in bar
       "maccy" # clipboard history
-      "choose-gui" # dmenu alternative
-      "http-toolkit"
+      "caffeine" # caffinate
+
+      "microsoft-teams"
+      "slack"
+      "signal"
+
+      "logseq"
+      "zed"
+      "wezterm"
+      "vscodium"
+
+      "firefox"
+      "google-chrome"
+      # "spotify"
+      # "overkill" # do not let Apple Music open
+
+      # "http-toolkit" # intercept http traffic
+      # "utm" # virtualization
+      "rectangle" # window management
     ];
   };
 }
