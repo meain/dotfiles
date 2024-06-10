ecae797aaa8df2f729333252ec5bc1e3e4b4ddac
Abin Simon <mail@meain.io>
2024-06-10 12:24:17 +0530

    [nix-darwin] update a bunch of find,docker and other settings

--- nix-darwin/.config/nix-darwin/flake.nix
+++ nix-darwin/.config/nix-darwin/flake.nix
@@ -19,16 +19,46 @@
           };
 
           dock = {
-            autohide = true;
-            mru-spaces = false;
-            orientation = "right";
-            show-recents = false;
-            tilesize = 10;
-            magnification = true;
-            wvous-tl-corner = 2;
-            wvous-bl-corner = 14;
+            autohide = false; # autohide dock
+            mru-spaces = false; # do not rearrange spaces based on most recent use
+            orientation = "right"; # dock on right side
+            show-recents = false; # do not show recently closed apps
+            tilesize = 10; # smallest icon size
+            magnification = true; # magnify icons on hover
 
-            persistent-apps = [ "/System/Applications/Utilities/Terminal.app" ];
+            # set hot corners
+            wvous-tl-corner = 2;
+            wvous-tr-corner = 2;
+            wvous-bl-corner = 1;
+            wvous-br-corner = 12;
+
+            persistent-apps = [ ];
+            # persistent-others = ["~/Desktop" "~/Downloads"];
+          };
+
+          finder = {
+            # AppleShowAllFiles = true; # show hidden files
+            CreateDesktop = false; # do not show icons on desktop
+            FXDefaultSearchScope = "SCcf"; # search current folder by default
+            ShowPathbar = true; # show path bar
+            ShowStatusBar = true; # show status bar
+            FXEnableExtensionChangeWarning = false; # do not warn when changing file extensions
+            FXPreferredViewStyle = "clmv"; # column view
+          };
+
+          menuExtraClock = {
+            ShowDate = 1; # show date in menu bar
+            ShowSeconds = true; # show seconds in menu bar
+          };
+
+          screencapture = {
+            location = "~/Documents/Screenshots"; # will have to create it manually
+          };
+
+          universalaccess = {
+            closeViewScrollWheelToggle = true; # enable zoom with scroll wheel
+            reduceMotion = true; # disable animations
+            reduceTransparency = true; # disable transparency
           };
         };
 
