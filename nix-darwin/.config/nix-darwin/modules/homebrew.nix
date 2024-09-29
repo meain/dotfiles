5372b1fd5921f40301a6a053db03fba355d310a0
Abin Simon <mail@meain.io>
2024-09-29 10:49:35 +0530

    [nix-darwin] homebrew: change emacs package being used

--- nix-darwin/.config/nix-darwin/modules/homebrew.nix
+++ nix-darwin/.config/nix-darwin/modules/homebrew.nix
@@ -5,17 +5,26 @@
 
     onActivation = {
       cleanup = "uninstall"; # uninstall any not listed here
-      # upgrade = true; # note idempotent anymore
+      # upgrade = true; # not idempotent anymore
     };
 
     global.brewfile = true;
 
     masApps = { }; # from mac app store
     taps = [
-      "railwaycat/emacsmacport"
+      # "railwaycat/emacsmacport"
+      # "d12frosted/emacs-plus"
     ];
     brews = [
-      "emacs-mac"
+      # {
+      #   name = "emacs-mac";
+      #   args = [ "with-natural-title-bar" "with-starter" "with-mac-metal" "with-native-comp"];
+      # }
+      # {
+      #   name = "emacs-plus@30";
+      #   args = [ "with-imagemagick" "with-native-comp" ];
+      #   link = true;
+      # }
       "choose-gui" # dmenu alternative
     ];
     casks = [
@@ -28,10 +37,14 @@
       "slack"
       "signal"
 
+      "emacs" # also emacs@pretest
+      # "emacs-app" # ???
       # "logseq"
       "obsidian"
       "ollama"
       # "lm-studio"
+      # "anythingllm"
+      # "cursor"
       "zed"
       "wezterm"
       "vscodium"
@@ -42,9 +55,10 @@
       # "spotify"
       # "overkill" # do not let Apple Music open
 
+      # "keycastr"
       # "http-toolkit" # intercept http traffic
       # "utm" # virtualization
-      "rectangle" # window management
+      # "rectangle" # window management
       "tailscale" # vpn
       "chatgpt" # quick shortcut is awesome
     ];
