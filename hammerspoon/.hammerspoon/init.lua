9aa75e03a0dba80c8ddbb9010b84febcd0d6dcee
Abin Simon <mail@meain.io>
2024-10-08 13:19:36 +0530

    [hammerspoon] use whisper and type out transcribed audio
    Now I can hit a keybinding and record the audio, get that transcribed,
    and then actually type in the thing that I transcribed using Hammerspoon.
    Checkout https://github.com/meain/raus

--- hammerspoon/.hammerspoon/init.lua
+++ hammerspoon/.hammerspoon/init.lua
@@ -177,4 +177,29 @@ hs.hotkey.bind(fkey, "g", centerMode)
 hs.hotkey.bind(fkey, "a", mainMode)
 hs.hotkey.bind(fkey, "d", sideMode)
 
+function transcribeAudio()
+    local path = io.open("/Users/meain/.local/share/latestpath"):read()
+    local task = hs.task.new("/bin/sh", function(exitCode, output, stdErr)
+        if exitCode ~= 0 then
+            hs.alert.show("Transcription failed: " .. (stdErr or "Unknown error"))
+            return
+        end
+
+        output = utils.trim(output)
+
+        if output == "." then
+            -- Just stopped previous one
+            return
+        elseif output == "" then
+            hs.alert.show("Speak up")
+        else
+            hs.eventtap.keyStrokes(output)
+        end
+    end, {"-c", "PATH='"..path.."' ,transcribe-audio"})
+
+    task:start()
+end
+
+hs.hotkey.bind(fkey, ";", transcribeAudio)
+
 hs.alert("Hammerspoon loaded!")
