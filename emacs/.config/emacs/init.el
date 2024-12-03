45f956cc4546a5bc88ad93ebfb565ae7f1557ac8
Abin Simon <mail@meain.io>
2024-12-03 20:47:53 +0530

    [emacs] gptel: remove refactor util funcs
    I never used it for code changes. Will add a more customized version
    if necessary at a later point.

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -4452,28 +4452,6 @@ For optional NO-CACHE, use caching by default."
     (interactive "sEnter website URL: ")
     (gptel-context-add-shell-command (format "readable %s" url) (not no-cache)))
 
-  ;; Pulled from https://github.com/karthink/gptel/wiki/Tweaking-LLM-responses
-  (cl-defun meain/clean-up-gptel-refactored-code (beg end)
-    "Clean up the code responses for refactored code in the current buffer.
-
-The response is placed between BEG and END.  The current buffer is
-guaranteed to be the response buffer."
-    (when gptel-mode          ; Don't want this to happen in the dedicated buffer.
-      (cl-return-from my/clean-up-gptel-refactored-code))
-    (when (and beg end)
-      (save-excursion
-        (let ((contents
-               (replace-regexp-in-string
-                "\n*``.*\n*" ""
-                (buffer-substring-no-properties beg end))))
-          (delete-region beg end)
-          (goto-char beg)
-          (insert contents))
-        ;; Indent the code to match the buffer indentation if it's messed up.
-        (indent-region beg end)
-        (pulse-momentary-highlight-region beg end))))
-  (add-hook 'gptel-post-response-functions #'my/clean-up-gptel-refactored-code)
-
   ;; https://github.com/karthink/gptel/wiki/Defining-custom-gptel-commands
   (defvar gptel-lookup--history nil)
   (defun gptel-lookup (prompt)
