e55b8cf25417bb39529d47fdd2e08a500c78a0c2
Abin Simon <mail@meain.io>
2025-02-28 11:29:09 +0530

    [emacs] add gptel aibo package

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -4395,6 +4395,9 @@ For optional NO-CACHE, use caching by default."
   :init
   (global-set-key (kbd "M-f i j") 'gptel-quick))
 
+(use-package gptel-aibo
+  :ensure (:host github :repo "dolmens/gptel-aibo"))
+
 (use-package llm :ensure t)
 (use-package yap
   :load-path "/Users/meain/dev/src/yap"
