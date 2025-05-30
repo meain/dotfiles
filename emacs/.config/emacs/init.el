0a9dff9810e0b0ba691ec875f3ab1778ca07604b
Abin Simon <mail@meain.io>
2025-05-30 09:56:25 +0530

    [emacs] update elpaca version

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -58,7 +58,7 @@
 (defvar github-models-api-key (get-api-key "github-models/apikey"))
 
 ;; Setup elpaca
-(defvar elpaca-installer-version 0.10)
+(defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
 (defvar elpaca-builds-directory (expand-file-name "builds/" elpaca-directory))
 (defvar elpaca-repos-directory (expand-file-name "repos/" elpaca-directory))
@@ -93,7 +93,7 @@
   (unless (require 'elpaca-autoloads nil t)
     (require 'elpaca)
     (elpaca-generate-autoloads "elpaca" repo)
-    (load "./elpaca-autoloads")))
+    (let ((load-source-file-function nil)) (load "./elpaca-autoloads"))))
 (add-hook 'after-init-hook #'elpaca-process-queues)
 (elpaca `(,@elpaca-order))
 
