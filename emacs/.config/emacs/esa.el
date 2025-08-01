e91736567d43c05f9e677eccff514d71a50e1f31
Abin Simon <mail@meain.io>
2025-08-01 15:48:10 +0530

    [emacs] esa: show output of an existing thread

--- emacs/.config/emacs/esa.el
+++ emacs/.config/emacs/esa.el
@@ -4,6 +4,7 @@
 ;; This package provides an Emacs client for the meain/esa agent framework.
 ;; Custom mode which will also syntax highlight tool/mcp calls, ie things that start with $ and # (not sure if it will conflict - or we can force it off here)
 ;; TODO: Using an actual termainl might allow us to also return confirmation stuff (or should we parse and show?)
+;; TODO: In case of an active selection, send that via stdin (should we send the filename as well?)
 
 ;;; Code:
 (defun esa--base (message agent continue)
@@ -46,9 +47,9 @@
   "Return a list of available conversations for esa."
   (seq-map (lambda (x)
              (cons
+              (s-trim (cadr (string-split x ": ")))
               (s-trim
-               (car (string-split x ": ")))
-              (s-trim (cadr (string-split x ": ")))))
+               (car (string-split x ": ")))))
            (seq-filter (lambda (x) (s-starts-with-p "  " x))
                        (string-split (shell-command-to-string "esa --list-history") "\n"))))
 
@@ -68,15 +69,39 @@ If `AGENT' is a universal-argument prefix, it will show a picker for the agent."
 If `PICK-CONVERSATION` is a universal-argument prefix, it will show a picker for the conversation."
   (interactive "sMessage: \nP")
   (let ((conversation (if (equal pick-conversation '(4))
-                          (completing-read "Select Conversation: " (esa--list-conversations))
+                          (cdr (assoc
+                                (completing-read "Select Conversation: " (esa--list-conversations))
+                                (esa--list-conversations)))
                         1)))
     (esa--base message nil conversation)))
 
+(defun esa-show-output (&optional pick-conversation)
+  "Show the output of one of the previous esa converstaions in a new buffer.
+If `PICK-CONVERSATION` is a universal-argument prefix, it will show a picker for the converstaion."
+  (interactive "P")
+  (let* ((converstaions (esa--list-conversations))
+         (converstaion (if (equal pick-conversation '(4))
+                     (cdr (assoc
+                           (completing-read "Select Conversation: " (esa--list-conversations))
+                           (esa--list-conversations)))
+                   (cdr (car converstaions)))))
+    (when converstaion
+      (let* ((buffer-name (format "*esa-output-%s*" converstaion))
+             (buffer (generate-new-buffer buffer-name)))
+        (with-current-buffer buffer
+          (let ((inhibit-read-only t))
+            (erase-buffer)
+            (insert (shell-command-to-string
+                     (format "esa --show-output %s" converstaion)))
+            (gfm-mode))
+          (goto-char (point-min)))
+        (display-buffer buffer)))))
 
 ;; Temporary
 (global-unset-key (kbd "M-e"))
 (global-set-key (kbd "M-e M-e") 'esa)
 (global-set-key (kbd "M-e M-c") 'esa-continue)
+(global-set-key (kbd "M-e M-o") 'esa-show-output)
 
 (provide 'esa)
 ;;; esa.el ends here
