65e025743a1bcb0b00bd607992b61c5b87c3c598
Abin Simon <mail@meain.io>
2025-12-31 10:45:05 +0530

    [emacs] rework and cleanup how fonts are set and updated

--- emacs/.config/emacs/lisp/visual.el
+++ emacs/.config/emacs/lisp/visual.el
@@ -6,23 +6,67 @@
 ;;; Code:
 ;; Set up fonts
 (use-package emacs
-  :commands (meain/select-font meain/what-font-am-i-using)
+  :commands (meain/set-font
+             meain/current-font
+             meain/reduce-font-size
+             meain/increse-font-size
+             meain/reset-font)
   :config
-  (defun meain/select-font ()
+  (defun meain/set-font (&optional name)
     "Select and set a font."
     (interactive)
-    (let ((font-name (completing-read "Choose font: " (cl-remove-duplicates (font-family-list)))))
-      (let ((family (meain/get-font-prop font-name 'family))
+    (let ((font-name (or name
+                         (completing-read "Choose font: " (cl-remove-duplicates (font-family-list))))))
+      (let ((height (meain/get-font-prop font-name 'height))
             (weight (meain/get-font-prop font-name 'weight)))
-        (set-frame-font family)
-        (set-face-attribute 'default nil :font family :weight weight)
-        (set-face-attribute 'fixed-pitch nil :font family :weight weight)
-        (set-face-attribute 'variable-pitch nil :font family :weight weight)
+        (message "Setting font to %s, height: %s, weight: %s" font-name height weight)
+        (set-frame-font font-name)
+        (set-face-attribute 'default nil :font font-name :weight weight :height height)
+        (set-face-attribute 'fixed-pitch nil :inherit 'default)
+        (set-face-attribute 'variable-pitch nil :inherit 'default)
         (setq-default line-spacing (meain/get-font-prop font-name 'line-spacing)))))
-  (defun meain/what-font-am-i-using ()
+  (defun meain/reduce-font-size (&optional reverse)
+    "Reduce the font size by 1 point. If REVERSE is non-nil, increase it instead."
+    (interactive "P")
+    (let* ((current-size (face-attribute 'default :height))
+           (new-size (if reverse
+                         (+ current-size 10)
+                       (- current-size 10))))
+      (set-face-attribute 'default nil :height new-size)
+      (set-face-attribute 'fixed-pitch nil :height new-size)
+      (set-face-attribute 'variable-pitch nil :height new-size)))
+  (defun meain/increse-font-size ()
+    "Increase the font size by 1 point."
+    (interactive)
+    (meain/reduce-font-size t))
+  (defun meain/change-font-weight ()
+    "Toggle between available font weights for the current font."
+    (interactive)
+    (let* ((weights (cl-remove-duplicates
+                     (mapcar (lambda (f)
+                               (font-get (font-spec :name f) :weight))
+                             (x-list-fonts (face-attribute 'default :family)))
+                     :test #'equal))
+           (current-weight (face-attribute 'default :weight))
+           (other-weights (cl-remove-if (lambda (w) (equal w current-weight)) weights))
+           (new-weight (intern (completing-read
+                                (format "New weight[%s]: " current-weight)
+                                other-weights nil t))))
+      (set-face-attribute 'default nil :weight new-weight)
+      (set-face-attribute 'fixed-pitch nil :weight new-weight)
+      (set-face-attribute 'variable-pitch nil :weight new-weight)))
+  (defun meain/reset-font ()
+    "Reset the font size to default size."
+    (interactive)
+    (let ((font-name (face-attribute 'default :family)))
+      (meain/set-font font-name)))
+  (defun meain/current-font ()
     "Show the name/details for the current font in use."
     (interactive)
-    (message "%s" (face-attribute 'default :font))))
+    (message "%s | height: %s | weight: %s"
+             (face-attribute 'default :family)
+             (face-attribute 'default :height)
+             (face-attribute 'default :weight))))
 
 ;; Bell: audio -> visual
 (use-package emacs
