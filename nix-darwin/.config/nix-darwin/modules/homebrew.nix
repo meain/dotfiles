d511af634323406fb4cace59a33bc7383ae6ffb8
Abin Simon <mail@meain.io>
2026-06-18 19:11:31 +0530

    [nix-darwin] clean up homebrew packages

--- nix-darwin/.config/nix-darwin/modules/homebrew.nix
+++ nix-darwin/.config/nix-darwin/modules/homebrew.nix
@@ -12,25 +12,12 @@
 
     masApps = { }; # from mac app store
     taps = [
-      # "railwaycat/emacsmacport"
       "d12frosted/emacs-plus"
       "zackelia/formulae" # for bclm
       "keith/formulae" # for reminders-cli
       "databricks/tap"
     ];
     brews = [
-      # Switching away from these builds onto installing the builds from jimeh/build-emacs-for-macos
-      # emacs-mac has not been updated in a long time
-      # {
-      #   name = "emacs-mac";
-      #   args = [ "with-natural-title-bar" "with-starter" "with-mac-metal" "with-native-comp"];
-      # }
-      # {
-      #   name = "emacs-plus@31";
-      #   args = [ "with-imagemagick" "with-native-comp" ];
-      #   link = true;
-      # }
-
       "choose-gui" # dmenu alternative
       "bclm" # limit battery charge
       "reminders-cli" # manage apple reminders from cli
@@ -46,31 +33,26 @@
       # "hiddenbar" # hide items in bar
       "maccy" # clipboard history
       "caffeine" # caffinate
+      "monitorcontrol" # control brightness of external monitors
 
       "microsoft-teams"
       "slack"
       "signal"
 
-      # "logseq"
       "obsidian"
       "ollama"
       # "lm-studio"
-      # "anythingllm"
-      # "cursor"
       "zed"
-      # "wezterm"
-      # "vscodium" # cody and cline
-      "visual-studio-code" # copilot
-      # "thunderbird"
+      "vscodium" # a gui editor
+      # "visual-studio-code" # copilot
       "orbstack" # better docker
       "kitty" # terminal emulator
 
       "firefox"
       "google-chrome"
-      # "spotify"
+      "spotify"
       # "overkill" # do not let Apple Music open
 
-      # "keycastr"
       # "http-toolkit" # intercept http traffic
       # "utm" # virtualization
       # "rectangle" # window management
@@ -83,9 +65,6 @@
       # ai agents
       "claude"
       "claude-code"
-      "copilot-cli"
-      "warp"
-      # "windsurf"
     ];
   };
 }
