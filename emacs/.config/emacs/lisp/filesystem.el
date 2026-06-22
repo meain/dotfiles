0feac66d7514a20d9600c7a21cd823f80119d0af
Abin Simon <mail@meain.io>
2026-06-22 11:42:55 +0530

    [emacs] dired: implement git-ignored file dimming
    Add dired-dim-git-ignores to visually de-emphasize files ignored by
    git/jj. Uses git ls-files to query ignored paths and overlays them
    with shadow face. Also fix unbalanced parenthesis in dired-mode-hook
    and remove commented-out old implementation.

--- emacs/.config/emacs/lisp/filesystem.el
+++ emacs/.config/emacs/lisp/filesystem.el
@@ -55,17 +55,46 @@
   (define-key evil-normal-state-map (kbd "_") 'find-file)
 
   (add-hook 'dired-mode-hook (lambda ()
-                               (setq imenu-generic-expression '((nil "\s+.+$" 0))))))
+                               (setq imenu-generic-expression '((nil "\s+.+$" 0)))))
 
-  ;; ;; TODO: make it work with directories
-  ;; (defun dired-dim-git-ignores ()
-  ;;   "Dim out .gitignore contents"
-  ;;   (when-let (((require 'vc))
-  ;;              ((require 'magit))
-  ;;              (ignores (magit-ignored-files))
-  ;;              (exts (make-local-variable 'completion-ignored-extensions)))
-  ;;     (dolist (item ignores) (add-to-list exts item))))
-  ;; (add-hook 'dired-mode-hook #'dired-dim-git-ignores)
+  (defun dired-dim-git-ignores ()
+    "Dim out git-ignored files in dired buffer."
+    (when-let* ((git-root (let ((jj-root (string-trim (shell-command-to-string "jj git root 2>/dev/null"))))
+                            (if (and jj-root (not (string-empty-p jj-root)))
+                                jj-root
+                              (when-let ((repo-root (locate-dominating-file default-directory ".git")))
+                                (expand-file-name ".git" repo-root)))))
+                ((file-directory-p git-root))
+                (work-tree (expand-file-name default-directory)))
+      (let* ((output (shell-command-to-string
+                      (concat "git --git-dir=" (shell-quote-argument git-root)
+                              " --work-tree=" (shell-quote-argument work-tree)
+                              " ls-files --ignored --exclude-standard --others --directory")))
+             (ignores (split-string output "\n" t)))
+        (save-excursion
+          (goto-char (point-min))
+          (while (not (eobp))
+            (when-let ((file (dired-get-filename 'relative 'noerror)))
+              (when (or (member file ignores)
+                        (member (concat file "/") ignores)
+                        ;; Handle directory entries
+                        (cl-some (lambda (ignore)
+                                   (string-prefix-p (file-name-as-directory file) ignore))
+                                 ignores))
+                (let ((ov (make-overlay (line-beginning-position)
+                                        (1+ (line-end-position)))))
+                  (overlay-put ov 'face 'shadow)
+                  (overlay-put ov 'dired-git-ignored t))))
+            (forward-line 1))))))
+  (defun dired-clear-git-ignore-overlays ()
+    "Remove git-ignore dimming overlays."
+    (remove-overlays (point-min) (point-max) 'dired-git-ignored t))
+  (add-hook 'dired-mode-hook
+            (lambda ()
+              (add-hook 'dired-after-readin-hook #'dired-dim-git-ignores nil t)))
+  (add-hook 'dired-mode-hook
+            (lambda ()
+              (add-hook 'before-revert-hook #'dired-clear-git-ignore-overlays nil t))))
 
 (use-package dired-x
   :after dired
@@ -88,10 +117,7 @@
     (dired (concat "/ssh:" (meain/ssh-host-picker) ":"))))
 
 ;; Enable recentf
-(use-package recentf
-  :defer t
-  :init
-  (recentf-mode t))
+(use-package recentf :defer t :init (recentf-mode t))
 
 ;; Save buffer
 (use-package emacs
